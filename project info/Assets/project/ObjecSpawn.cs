edd03606f53fa220c053869049db009d8f567e14
nwemmer1 <35506038+nwemmer1@users.noreply.github.com>
2019-11-16 17:48:08 -0500

    Reupload project in project info folder

---
+++ project info/Assets/project/ObjecSpawn.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scorer : MonoBehaviour
+{
+    // Start is called before the first frame update
+	//public GameObject score;
+	//public text scoreText;
+
+	//void onTriggerEnter()
+	//{
+//		int currScore = int.Parse(score.GetComponent().text) +1;
+//		score.GetComponent().text = currScore.ToString();
+	//}
+
+}
---
+++ project info/Assets/project/keepScore.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keepScore : MonoBehaviour
+{
+    public static int Score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect(100, 100, 100, 100), Score.ToString());
+    }
+}
